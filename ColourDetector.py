19bd164784d536baca927f1802e5639b50468ad1
Shraman Biswas <shraman-biswas@users.noreply.github.com>
2016-03-06 19:44:25 +0530

    formatted file

--- ColourDetector.py
+++ ColourDetector.py
@@ -7,7 +7,10 @@ WHITE = (255,255,255)
 
 class ColourDetector:
 
+	# detected colours list
 	colours = []
+
+	# colours table
 	_colour_table = [
 		("white",	(255,255,255)),
 		("black",	(0,0,0)),
@@ -19,6 +22,8 @@ class ColourDetector:
 		#("cyan",	(255,255,0)),
 		#("magenta",(255,0,255)),
 	]
+
+	# convert colours table from BGR to CIELAB colour space
 	_colour_lab = np.array([c[1] for c in _colour_table]) \
 					.reshape(len(_colour_table),1,3) \
 					.astype(np.uint8)
@@ -27,16 +32,21 @@ class ColourDetector:
 	def __init__(self, contours, img):
 		# convert image to CIELAB colour space
 		cielab_img = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
 		# create list of colour labels
-		self.colours = [self._detect_colour(cnt, cielab_img) for cnt in contours]
+		self.colours = [self._detect_colour(cnt, cielab_img)
+			for cnt in contours]
 
 	def _detect_colour(self, cnt, img):
 		# create a mask for each contour
 		mask = np.zeros(img.shape[:2], dtype=np.uint8)
 		cv2.drawContours(mask, [cnt], 0, WHITE, -1)
 		mask = cv2.erode(mask, None, iterations=2)
+
 		# calculate mean CIELAB colour of each contour
 		mean_colour = cv2.mean(img, mask=mask)[:3]
+
 		# search for colour in colour table
-		idx = np.argmin([np.linalg.norm(mean_colour - c) for c in self._colour_lab])
+		idx = np.argmin([np.linalg.norm(mean_colour - c)
+			for c in self._colour_lab])
 		return self._colour_table[idx][0]
